8c72f592ac7c829de7b6097c373d8223523c9cb9
Philsik Chang <phil@mrl.snu.ac.kr>
2021-08-06 14:54:32 +0900

    Trying out package_data options?

--- setup.py
+++ setup.py
@@ -8,18 +8,6 @@ VERSION = 0.1
 
 setup_py_dir = os.path.dirname(os.path.realpath(__file__))
 
-need_files = []
-datadir = "pybulletgym/envs/assets"
-
-hh = setup_py_dir + "/" + datadir
-
-for root, dirs, files in os.walk(hh):
-    for fn in files:
-        ext = os.path.splitext(fn)[1][1:]
-        if ext and ext in 'png gif jpg urdf sdf obj mtl dae off stl STL xml '.split():
-            fn = root + "/" + fn
-            need_files.append(fn[1+len(hh):])
-
 setup(name='pybulletgym',
       version=VERSION,
       description='PyBullet Gym is an open-source implementation of the OpenAI Gym MuJoCo environments for use with the OpenAI Gym Reinforcement Learning Research Platform in support of open research.',
@@ -33,5 +21,5 @@ setup(name='pybulletgym',
       install_requires=[
           'pybullet>=1.7.8',
       ],
-      package_data={'pybulletgym': need_files},
+      package_data={'pybulletgym': ["*.png", "*.gif", "*.jpg", "*.urdf", "*.sdf", "*.obj", "*.mtl", "*.dae", "*.off", "*.stl", "*.STL", "*.xml"]},
 )
